a5e5f4a88f0d07724c8edc4f472dad07f876edd5
meysamzi <meysamzeynali2@gmail.com>
2022-08-19 23:14:24 +0430

    Script

--- Components/Dogs/Ads.js
+++ Components/Dogs/Ads.js
@@ -6,7 +6,7 @@ const Ads = () => {
 
     return <section>
         <div className="flex rounded max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto my-10 lg:my-28">
-            <ul className="basis-[60%] flex flex-col gap-6 bg-red-600 text-white p-4 rounded-r">
+            <ul className="basis-[60%] flex flex-col items-center gap-6 bg-red-600 text-white p-4 rounded-r">
                 {items?.map((i, index) =>
                     <li key={index}>
                         <span>
