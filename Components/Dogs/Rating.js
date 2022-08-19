0ae22d9af0791a40acab7be64d17a8dc1a9f9e02
meysamzi <meysamzeynali2@gmail.com>
2022-08-19 23:30:41 +0430

    Script

--- Components/Dogs/Rating.js
+++ Components/Dogs/Rating.js
@@ -9,7 +9,7 @@ const Rating = () => {
     return <section>
         <div className="flex flex-col gap-8 max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto">
             {items?.map((i, index) =>
-                <div key={index} className={`flex gap-4 p-4 rounded ${index === 0 && "bg-black text-white"}`}>
+                <div key={index} className={`flex gap-4 items-center p-4 rounded ${index === 0 && "bg-black text-white"}`}>
                     <div className="basis-[50%]">
                         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت
                     </div>
