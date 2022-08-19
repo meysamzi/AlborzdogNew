74d8c48280a9a0aa4c6a4a1d261b7cba16e6167d
meysamzi <meysamzeynali2@gmail.com>
2022-08-19 21:31:27 +0430

    Script

--- Components/Share/NavBar.js
+++ Components/Share/NavBar.js
@@ -7,6 +7,8 @@ const NavBar = () => {
 
     const items = [1, 2, 3, 4, 56, 5, 5, 5, 5, 5, 5, 55, 5, 5, 55, 5, 5, 5, 5, 5]
 
+    const attitudes = [1, 2, 3, 4, 5]
+
     return <nav className="bg-red-600 relative">
         <div className="flex gap-x-2 items-center max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto">
             <ul className="basis-[80%] flex text-white">
@@ -18,7 +20,7 @@ const NavBar = () => {
                             </a>
                         </Link>
                         <div className="hidden z-10 group-hover:flex absolute right-0 left-0 mx-4 rounded-b bg-white text-black shadow-2xl p-4">
-                            <ul className="basis-[20%] grid grid-cols-3 gap-8">
+                            <ul className="basis-[15%] grid grid-cols-3 gap-8">
                                 <h6 className="font-bold col-span-full">
                                     لیست کل نژاد ها
                                 </h6>
@@ -32,8 +34,18 @@ const NavBar = () => {
                                     </li>
                                 )}
                             </ul>
-                            <div className="basis-[80%]">
-                                a
+                            <div className="basis-[85%] flex gap-8">
+                                <ul className="basis-[50%] flex flex-col gap-8">
+                                    {attitudes?.map((i, index) =>
+                                        <li className={`${index === 0 && "font-bold"}`}>
+                                            تست تست تست
+                                        </li>
+                                    )}
+                                </ul>
+                                <ul className="basis-[50%]">
+                                    <li>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </li>
