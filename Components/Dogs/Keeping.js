ca2a805234c878a6948a3fb1e93d483985977d18
meysamzi <meysamzeynali2@gmail.com>
2022-08-20 00:27:13 +0430

    Script

--- Components/Dogs/Keeping.js
+++ Components/Dogs/Keeping.js
@@ -5,7 +5,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 const Keeping = () => {
 
     return <section>
-        <div className="flex gap-10 max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto">
+        <div className="flex gap-10 shadow p-4 max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto">
             <div className="basis-[30%] flex flex-col gap-8">
                 <span className="font-bold">نگهداری</span>
                 <div className="relative w-full h-[250px]">
