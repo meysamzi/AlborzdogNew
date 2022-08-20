2d26d65cadb9975d281e772f1db6845507ea45f3
meysamzi <meysamzeynali2@gmail.com>
2022-08-20 15:48:49 +0430

    Script

--- Components/Dogs/Keeping.js
+++ Components/Dogs/Keeping.js
@@ -10,7 +10,7 @@ const Keeping = () => {
         <div className="flex gap-10 shadow p-4 max-w-7xl mx-4 md:mx-6 lg:mx-8 xl:mx-10 2xl:mx-auto">
             <div className="basis-[30%] flex flex-col gap-8">
                 <span className="font-bold">نگهداری</span>
-                <div className="relative w-full h-[250px]">
+                <div className="relative h-[250px]">
                     <Image src={'/Images/Doberman.jpg'} alt={"pic"} className='object-cover rounded' layout="fill" />
                 </div>
             </div>
